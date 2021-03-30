e5190e98d4a962235963ea91514e78ac7249d313
poran200 <chowdury659@gmail.com>
2021-03-30 21:44:13 +0600

    flux and mono api controller

---
+++ src/main/java/com/learnractive/reactive/controller/FluxAndMonoController.java
@@ -0,0 +1,27 @@
+package com.learnractive.reactive.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+@RestController
+@RequestMapping("api/")
+public class FluxAndMonoController {
+    @GetMapping(value = "/flux")
+    public ResponseEntity<Object> returnFlux(){
+        return ResponseEntity.ok().body(Flux.just(1,2,3,4)
+                .delayElements(Duration.ofSeconds(1))
+                .log());
+    }
+    @GetMapping(value = "/fluxstrem",produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public ResponseEntity<Object> returnFluxStrem(){
+        return ResponseEntity.ok().body(Flux.just(1,2,3,4)
+                .delayElements(Duration.ofSeconds(1))
+                .log());
+    }
+}
