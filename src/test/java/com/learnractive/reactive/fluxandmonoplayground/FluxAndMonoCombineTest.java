00e2a6d3795418e4003914527812e995e726fcd8
poran200 <chowdury659@gmail.com>
2021-03-30 17:50:41 +0600

    flux and mono combine test

---
+++ src/test/java/com/learnractive/reactive/fluxandmonoplayground/FluxAndMonoCombineTest.java
@@ -0,0 +1,71 @@
+package com.learnractive.reactive.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+class FluxAndMonoCombineTest {
+    @Test
+    void combineUsingMarge() {
+        var flux1 = Flux.just("A", "B", "C");
+        var flux2 = Flux.just("E", "F", "G");
+        var merge = Flux.merge(flux1, flux2);
+        StepVerifier.create(merge.log())
+                .expectSubscription()
+                .expectNextCount(6)
+                .verifyComplete();
+
+    }
+
+     @Test
+     void combineUsingMarge_with_Delay() {
+         var flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
+         var flux2 = Flux.just("E", "F", "G").delayElements(Duration.ofSeconds(1));
+         var merge = Flux.merge(flux1, flux2);
+         StepVerifier.create(merge.log())
+                 .expectSubscription()
+                 .expectNextCount(6)
+                 .verifyComplete();
+
+     }
+
+
+    @Test
+    void combineUsingConcat() {
+        var flux1 = Flux.just("A", "B", "C");
+        var flux2 = Flux.just("E", "F", "G");
+        var merge = Flux.concat(flux1, flux2);
+        StepVerifier.create(merge.log())
+                .expectSubscription()
+                .expectNextCount(6)
+                .verifyComplete();
+
+    }
+    @Test
+    void combineUsingConcat_with_Delay() {
+        var flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
+        var flux2 = Flux.just("E", "F", "G").delayElements(Duration.ofSeconds(1));
+        var merge = Flux.concat(flux1, flux2);
+        StepVerifier.create(merge.log())
+                .expectSubscription()
+                .expectNextCount(6)
+                .verifyComplete();
+
+    }
+
+    @Test
+    void combineUsingZip() {
+        var flux1 = Flux.just("A", "B", "C");
+        var flux2 = Flux.just("E", "F", "G"); 
+        var merge = Flux.zip(flux1, flux2, String::concat);
+        StepVerifier.create(merge.log())
+                .expectSubscription()
+                .expectNextCount(3)
+                .verifyComplete();
+
+    }
+
+
+}
