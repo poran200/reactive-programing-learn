b286bc5c518d856dff398573138b0a1da9c88a9b
poran200 <chowdury659@gmail.com>
2021-03-31 17:28:29 +0600

    flux and mono api functional route  and handler test

---
+++ src/test/java/com/learnractive/reactive/handeller/SampleHandelerFunctionTest.java
@@ -0,0 +1,34 @@
+package com.learnractive.reactive.handeller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class SampleHandelerFunctionTest {
+    @Autowired
+    WebTestClient webTestClient;
+    @Test
+    void flux() {
+
+        var responseEntityFlux = webTestClient.get().uri("http://localhost:8080/functional/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(responseEntityFlux)
+                .expectSubscription()
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .verifyComplete();
+    }
+}
